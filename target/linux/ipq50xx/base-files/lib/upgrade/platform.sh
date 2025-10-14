cdddcdf27c35e79cb73d9bddf68a53c68a097193
sqliuchang <jssqliuchang@gmail.com>
2025-10-14 17:02:34 +0800

    ipq50xx: Add CMCC PZ-L8 support

--- target/linux/ipq50xx/base-files/lib/upgrade/platform.sh
+++ target/linux/ipq50xx/base-files/lib/upgrade/platform.sh
@@ -1,7 +1,32 @@
 . /lib/functions.sh
 
-RAMFS_COPY_BIN='fw_printenv fw_setenv'
+#RAMFS_COPY_BIN='fw_printenv fw_setenv'
+RAMFS_COPY_BIN='dumpimage fw_printenv fw_setenv head seq'
 RAMFS_COPY_DATA='/etc/fw_env.config /var/lock/fw_printenv.lock'
+
+remove_oem_ubi_volume() {
+	local oem_volume_name="$1"
+	local oem_ubivol
+	local mtdnum
+	local ubidev
+
+	mtdnum=$(find_mtd_index "$CI_UBIPART")
+	if [ ! "$mtdnum" ]; then
+		return
+	fi
+
+	ubidev=$(nand_find_ubi "$CI_UBIPART")
+	if [ ! "$ubidev" ]; then
+		ubiattach --mtdn="$mtdnum"
+		ubidev=$(nand_find_ubi "$CI_UBIPART")
+	fi
+
+	if [ "$ubidev" ]; then
+		oem_ubivol=$(nand_find_volume "$ubidev" "$oem_volume_name")
+		[ "$oem_ubivol" ] && ubirmvol "/dev/$ubidev" --name="$oem_volume_name"
+	fi
+}
+
 
 platform_check_image() {
 	local board=$(board_name)
@@ -10,6 +35,9 @@ platform_check_image() {
 		xiaomi,cr881x)
 			mi_dualboot_check_image "$1"
 			return $?
+			;;
+		cmcc,pz-l8)
+			return 0;
 			;;
 		*)
 			v "Sysupgrade is not supported on your board($board) yet."
@@ -21,6 +49,20 @@ platform_check_image() {
 platform_do_upgrade() {
 	local board=$(board_name)
 	case $board in
+		cmcc,pz-l8)
+			local delay
+
+			delay=$(fw_printenv bootdelay)
+			[ -z "$delay" ] || [ "$delay" -eq "0" ] && \
+				fw_setenv bootdelay 3
+
+			elecom_upgrade_prepare
+
+			remove_oem_ubi_volume bt_fw
+			remove_oem_ubi_volume ubi_rootfs
+			remove_oem_ubi_volume wifi_fw
+			nand_do_upgrade "$1"
+			;;
 		redmi,ax3000|\
 		xiaomi,cr881x)
 			mi_dualboot_do_upgrade "$1"
